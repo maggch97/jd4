0b9382e9a4d1f18dc926b911b309cc2abb57437c
Weijie Zhao <maggch@outlook.com>
2020-03-09 13:50:51 +0000

    添加井字棋

--- judge.py
+++ judge.py
@@ -2,6 +2,7 @@ import json
 from asyncio import sleep
 import aiohttp
 from gomoku.gomoku_referee import GomokuReferee
+from gomoku.tic_tac_toe_referee import ticTacToeReferee
 from gomoku.referee import Referee, Player
 from jd4.compile import build
 from asyncio import get_event_loop, gather
@@ -44,38 +45,45 @@ async def judge():
             time_limit_ns = judgeData["TimeLimitNs"]
             memory_limit_bytes = judgeData["MemoryLimitBytes"]
             if gameID == 1:
-                time1 = time.time()
                 r = GomokuReferee(
                     [Player(0, player["CodeContent"]["Language"],
                             player["CodeContent"]["Content"]) for player in players],
                     time_limit_ns,
                     memory_limit_bytes)
-                await r.run()
-                time2 = time.time()
-                postData = {
-                    "ID": match_id,
-                    "Token": judgeData["JudgeToken"],
-                    "MatchStates": [
-                        {
-                            "PlayersScore": json.dumps(state.players_score),
-                            "Index": state.index,
-                            "Detail": state.detail,
-                            "Status": state.status,
-                            "InputData": state.input_data,
-                            "OutputData": state.output_data,
-                            "TimeUsageNs": state.time_usage_ns,
-                            "MemoryUsageBytes": state.memory_usage_bytes,
-                        }
-                        for state in r.states
-                    ]
-                }
-                judge_count = judge_count+1
-                async with aiohttp.ClientSession() as session:
-                    r = await session.post(api+"/api/judge?access_token={}".format(access_token), json=postData)
-                    response = await r.text(encoding="utf-8")
-                time3 = time.time()
-                print(time2-time1, time3-time2, time1,
-                      time2, judge_count, flush=True)
+
+            elif gameID == 2:
+                r = ticTacToeReferee(
+                    [Player(0, player["CodeContent"]["Language"],
+                            player["CodeContent"]["Content"]) for player in players],
+                    time_limit_ns,
+                    memory_limit_bytes)
+            time1 = time.time()
+            await r.run()
+            time2 = time.time()
+            postData = {
+                "ID": match_id,
+                "Token": judgeData["JudgeToken"],
+                "MatchStates": [
+                    {
+                        "PlayersScore": json.dumps(state.players_score),
+                        "Index": state.index,
+                        "Detail": state.detail,
+                        "Status": state.status,
+                        "InputData": state.input_data,
+                        "OutputData": state.output_data,
+                        "TimeUsageNs": state.time_usage_ns,
+                        "MemoryUsageBytes": state.memory_usage_bytes,
+                    }
+                    for state in r.states
+                ]
+            }
+            judge_count = judge_count+1
+            async with aiohttp.ClientSession() as session:
+                r = await session.post(api+"/api/judge?access_token={}".format(access_token), json=postData)
+                response = await r.text(encoding="utf-8")
+            time3 = time.time()
+            print(time2-time1, time3-time2, time1,
+                  time2, judge_count, flush=True)
         except Exception as e:
             print("Exception : ", e, flush=True)
             await sleep(3)
